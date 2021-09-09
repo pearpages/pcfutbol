efd59a8bbc29f9325b428e009dbce4054ba8ef56
Pere Pages <pere.pages@ocado.com>
2021-09-09 19:53:44 +0200

    fix: Classification html

--- src/Classification/Classification.tsx
+++ src/Classification/Classification.tsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from "react";
 
-import { Team } from '../lib/createSquads'
+import { Team } from "../lib/createSquads";
 
-export function Classification({teams: originalTeams}: {teams: Team[]}) {
+export function Classification({ teams: originalTeams }: { teams: Team[] }) {
   const teams = originalTeams.slice().sort((a, b) => b.points - a.points);
 
-  return (<div>
-    <h4>Classification</h4>
-    <table>
-      <thead>
-        <th>Team</th>
-        <th>Games</th>
-        <th>Points</th>
-      </thead>
-      <tbody>
-        {teams.map(team => (<tr key={team.name}>
-          <td>{team.name}</td>
-          <td>{team.games}</td>
-          <td>{team.points}</td>
-        </tr>))}
-      </tbody>
-    </table>
-  </div>)
+  return (
+    <div>
+      <h4>Classification</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Team</th>
+            <th>Games</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map((team) => (
+            <tr key={team.name}>
+              <td>{team.name}</td>
+              <td>{team.games}</td>
+              <td>{team.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
